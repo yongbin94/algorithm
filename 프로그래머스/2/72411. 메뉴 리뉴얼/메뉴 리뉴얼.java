6527107aa00d2cd72325bdb7b49b6409e1316ae7
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2024-04-25 11:32:46 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 284.03 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int[] O, C, max;
+    static List<String>[] list;
+    public String[] solution(String[] orders, int[] course) {
+        max = new int[27];
+        list = new ArrayList[27];
+        for(int i = 0; i <= 26; i++)
+            list[i] = new ArrayList<>(); 
+        O = new int[orders.length];
+        C = course;
+        for(int i = 0; i < orders.length; i++) {
+            int bit = 0;
+            for(char c : orders[i].toCharArray())
+                bit += 1 << (c - 'A');
+            O[i] = bit;
+        }
+        System.out.println(26);
+        recur(0,0, 0);
+        
+        int size = 0, idx = 0;
+        for(List<String> l : list)
+            size += l.size();
+        
+        String[] answer = new String[size];
+        for(List<String> l : list)
+            for(String str : l)
+                answer[idx++] = str;
+        Arrays.sort(answer);
+        return answer;
+    }
+    static void recur(int cnt, int start, int bit) {
+        for(int c : C) {
+            if(c == cnt) {
+                int tmp = 0;
+                for(int o : O) {
+                    if((bit & o) == bit)
+                        tmp++;
+                }
+                if(tmp >= 2 && max[cnt] <= tmp) {
+                    StringBuilder sb = new StringBuilder();
+                    for(int i = 0; i < 26; i++) {
+                        if(((bit >> i) & 1) == 1)
+                            sb.append((char)('A' + i));
+                    }
+                    if(max[cnt] < tmp) {
+                        max[cnt] = tmp;
+                        list[cnt] = new ArrayList<>();
+                    }
+                    list[cnt].add(sb.toString());
+                }
+                if(c == C[C.length - 1])
+                    return;
+                break;
+            }
+        }
+        for(int i = start; i < 26; i++) 
+            recur(cnt + 1, i + 1, bit + (1 << i));
+    }
+}
