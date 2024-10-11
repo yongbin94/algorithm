b506db00446e5b6b626372c4cc94de0c5e8c9ad8
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2024-10-11 13:20:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 24.69 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        char[] A = new_id.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : A) {
+            if (ch >= 'A' && ch <= 'Z')
+                ch = (char) (ch - 'A' + 'a');
+            if (!(ch >= 'a' && ch <= 'z') && !(ch >= '0' && ch <= '9') && !(ch == '-' || ch == '_' || ch == '.'))
+                continue;
+            if(sb.length() != 0)
+                System.out.println(sb.length() + " " + sb.charAt(sb.length() - 1));
+            if(ch == '.'  && (sb.length() == 0 || sb.charAt(sb.length() - 1) == '.'))
+                continue;
+            sb.append(ch);
+        }
+        
+        if(sb.length() > 15) {
+            sb.delete(15, sb.length());
+        }
+        
+        for (int i = sb.length() - 1; i >= 0; i--) {
+            if(sb.charAt(i) == '.')
+                sb.deleteCharAt(i);
+            else
+                break;
+        }
+        
+        if (sb.length() == 0)
+            sb.append('a');
+        
+        while(sb.length() <= 2)
+            sb.append(sb.charAt(sb.length() - 1));
+        
+        return sb.toString();
+    }
+}
