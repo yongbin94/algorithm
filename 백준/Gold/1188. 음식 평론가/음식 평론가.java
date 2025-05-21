232aa00b5e0fcad9a3eaa4e09283faf65ddb7d2c
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2025-05-21 15:03:58 +0900

    [Gold IV] Title: 음식 평론가, Time: 172 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(M - gcd(N, M));
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
