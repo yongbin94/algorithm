4abf6b4c6626fbfcd275737d70685749ba36912e
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2025-09-26 16:31:49 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,15 @@
+SELECT APNT_NO,
+       PT_NAME,
+       PT_NO,
+       A.MCDP_CD AS MCDP_CD,
+       DR_NAME,
+       APNT_YMD
+FROM   APPOINTMENT A
+       JOIN PATIENT USING(PT_NO)
+       JOIN DOCTOR 
+            ON MDDR_ID = DR_ID
+WHERE  A.MCDP_CD = 'CS'
+       AND APNT_YMD >= DATE '2022-04-13'
+       AND APNT_YMD < DATE '2022-04-14'
+       AND APNT_CNCL_YN = 'N'
+ORDER  BY APNT_YMD
