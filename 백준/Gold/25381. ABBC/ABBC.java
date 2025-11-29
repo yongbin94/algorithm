3de3dc0637d47a1d06e00d994e6df09bee6ffc57
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2025-11-29 19:56:32 +0900

    [Gold III] Title: ABBC, Time: 236 ms, Memory: 28052 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.java
+++ 백준/Gold/25381. ABBC/ABBC.java
@@ -2,23 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<Character> st = new ArrayDeque<>();
+        char[] A = br.readLine().toCharArray();
         int answer = 0;
-        for (char c : br.readLine().toCharArray()) {
-            if (c == 'A') st.push(c);
-            else if (c == 'B') {
-                if (!st.isEmpty() && st.peek() == 'A') {
-                    st.pop();
-                    answer++;
-                } else st.push(c);
-            } else {
-                if (!st.isEmpty() && st.peek() == 'B') {
-                    st.pop();
-                    answer++;
-                } else st.push(c);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 'B') q.add(i);
+            else if (A[i] == 'C' && !q.isEmpty()) {
+                q.poll();
+                answer++;
             }
+        }
+        for (int i = 0; i < A.length && !q.isEmpty(); i++) {
+            if (A[i] != 'A') continue;
+            while (!q.isEmpty() && q.peek() < i) q.poll();
+            if (q.isEmpty()) break;
+            q.poll();
+            answer++;
         }
         System.out.println(answer);
     }
