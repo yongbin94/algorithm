db2c02e087449a62b5fe9b3e4612d321f8b720aa
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2025-12-21 13:27:25 +0900

    [Gold V] Title: 영정이의 청소, Time: 304 ms, Memory: 37016 KB -BaekjoonHub

---
+++ 백준/Gold/14671. 영정이의 청소/영정이의 청소.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int res = 0;
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            res |= 1 << (((x & 1) << 1) | ((x ^ y) & 1));
+        }
+        System.out.println(res == 15 ? "YES" : "NO");
+    }
+}
