022a8c6672ee53e48c8d16f1de0864c536354d93
Yongbin Park <156792910+yongbin94@users.noreply.github.com>
2026-04-16 04:32:47 +0900

    [Gold IV] Title: LCS 3, Time: 148 ms, Memory: 20396 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		String s3 = br.readLine();
+
+		int n1 = s1.length();
+		int n2 = s2.length();
+		int n3 = s3.length();
+
+		int[][][] dp = new int[n1 + 1][n2 + 1][n3 + 1];
+
+		for (int i = 1; i <= n1; i++) {
+			for (int j = 1; j <= n2; j++) {
+				for (int k = 1; k <= n3; k++) {
+					if (s1.charAt(i - 1) == s2.charAt(j - 1) && s2.charAt(j - 1) == s3.charAt(k - 1)) {
+						dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+					} else {
+						dp[i][j][k] = Math.max(dp[i - 1][j][k], Math.max(dp[i][j - 1][k], dp[i][j][k - 1]));
+					}
+				}
+			}
+		}
+		System.out.println(dp[n1][n2][n3]);
+	}
+}
